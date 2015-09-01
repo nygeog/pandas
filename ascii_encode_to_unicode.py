bc5716e0447922f74848e73a66379eae1e983187
Daniel M Sheehan <daniel.martin.sheehan@gmail.com>
2015-09-01 12:26:28 -0700

    update

---
+++ ascii_encode_to_unicode.py
@@ -0,0 +1,4 @@
+df['name'] = df.name.str.encode('ascii', 'ignore')
+
+
+#http://stackoverflow.com/questions/2365411/python-convert-unicode-to-ascii-without-errors
---
+++ create_index.py
@@ -0,0 +1,1 @@
+df['index_col'] = df.index
