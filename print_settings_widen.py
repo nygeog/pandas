bd609e2c4ff172199f2b5da6fc34b5762189e62a
Danny S <danielmsheehan@iMac.local>
2016-05-29 20:00:21 -0400

    added df widen

---
+++ print_settings_widen.py
@@ -0,0 +1,4 @@
+pd.options.display.max_columns = 5200
+pd.options.display.max_rows    = 5200
+pd.set_option('display.width', 1000) #http://stackoverflow.com/questions/11707586/python-pandas-widen-output-display
+pd.set_option('display.max_rows', 500) #http://stackoverflow.com/questions/11707586/python-pandas-widen-output-display
