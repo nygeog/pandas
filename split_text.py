669ac76ad2f0b0453557a1184afc68f26957cae8
Danny S <dsheehan@cartodb.com>
2016-09-01 17:27:27 -0400

    update split text

--- split_text.py
+++ split_text.py
@@ -1,2 +1,6 @@
 df_trim['hn_rnd2'] = df_trim.MATCH_ADDR.str.split(' ',1).str[0]
 df_trim['street_rnd2'] = (df_trim.MATCH_ADDR.str.split(',',1).str[0]).str.split(' ',1).str[1]
+
+
+df['latitude'] = df.return_address_str.str.split('*',2).str[1]
+df['longitude'] = df.return_address_str.str.split('*',2).str[2]
